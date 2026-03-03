23675ae71a911ec8ac8d99e896bf1229d595529c
Jonathan Peros <jperos@jenova.games>
2026-03-03 23:23:49 +0000

    Decompile Sound_CopyAndRelocateInstruments

--- src/slps_023.64/system/spu.c
+++ src/slps_023.64/system/spu.c
@@ -3,7 +3,18 @@
 #include "system/sound.h"
 
 //----------------------------------------------------------------------------------------------------------------------
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/spu", Sound_CopyAndRelocateInstruments);
+void Sound_CopyAndRelocateInstruments( FSoundInstrumentInfo* in_A, FSoundInstrumentInfo* in_B, s32 in_AddrOffset, s32 in_Count )
+{
+    do {
+        in_B->StartAddr = in_A->StartAddr + in_AddrOffset;
+        in_B->LoopAddr = in_A->LoopAddr + in_AddrOffset;
+        *(s32*)&in_B->FineTune = *(s32*)&in_A->FineTune;
+        *(s32*)&in_B->AdsrLower = *(s32*)&in_A->AdsrLower;
+        in_A++;
+        in_B++;
+        in_Count--;
+    } while( in_Count != 0 );
+}
 
 //----------------------------------------------------------------------------------------------------------------------
 bool Sound_IsNotAkaoFile( FAkaoFileBlob* in_Blob )
