6a12f516a7a727ec959a959d3b368cee1974e2ec
AKCore <dblegendsmud@gmail.com>
2026-03-13 21:16:30 -0700

    replace renamed values with correct ones so it builds again

--- src/slps_023.64/system/sound.c
+++ src/slps_023.64/system/sound.c
@@ -911,7 +911,7 @@ void func_8004CA1C(FSoundChannel* in_pChannel )
         {
             int Temp;
             
-            Temp = (in_pChannel->ChannelPan + in_pChannel->field41_0x80) >> 8;
+            Temp = (in_pChannel->ChannelPan + in_pChannel->D_Volume_Value) >> 8;
             Temp += in_pChannel->AutoPanVolume;
             Temp += 0x80;
 
@@ -941,7 +941,7 @@ void func_8004CA1C(FSoundChannel* in_pChannel )
     if( UpdateFlags & 0x10 )
     {
         var_a1 = *((u16*)in_pChannel - 0x6) + in_pChannel->VibratoPitch + (in_pChannel->PitchSlide >> 16);
-        temp_a0_4 = in_pChannel->E_Value & 0xFF00;
+        temp_a0_4 = in_pChannel->E_SampleRate_Value & 0xFF00;
         if (!(in_pChannel->unk_Flags & SOUND_CHANNEL_UNK_FLAGS_25))
         {
             temp_a0_5 = temp_a0_4 >> 8;
@@ -963,7 +963,7 @@ void func_8004CA1C(FSoundChannel* in_pChannel )
     else if( in_pChannel->VoiceParams.VoiceParamFlags & 0x10 )
     {
         var_a1_2 = in_pChannel->PitchBase + in_pChannel->VibratoPitch + (in_pChannel->PitchSlide >> 16);
-        temp_a0_6 = in_pChannel->E_Value & 0xFF00;
+        temp_a0_6 = in_pChannel->E_SampleRate_Value & 0xFF00;
         if (!(in_pChannel->unk_Flags & SOUND_CHANNEL_UNK_FLAGS_25))
         {
             temp_a0_7 = temp_a0_6 >> 8;
