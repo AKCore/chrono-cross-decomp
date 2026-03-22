6f219410c63f3f58c2afe593587bafa8b8d32093
Jonathan Peros <jperos@jenova.games>
2026-03-21 21:54:56 -0400

    Changing sound commands to an enum for better clangd type completion

--- include/system/soundCommand.h
+++ include/system/soundCommand.h
@@ -3,48 +3,55 @@
 
 #include "common.h"
 
-#define SOUND_CMD_10_START_FIELD_MUSIC                   ( 0x10U )
-#define SOUND_COMMAND_UNK_11                             ( 0x11U )
-#define SOUND_CMD_12_START_FIELD_MUSIC_LOOPED            ( 0x12U )
-#define SOUND_CMD_14_START_BATTLE_MUSIC                  ( 0x14U )
-#define SOUND_CMD_19_SET_MUSIC_LEVEL_IMM                 ( 0x19U )
-#define SOUND_CMD_20_UNK                                 ( 0x20U )
-#define SOUND_CMD_21_EVICT_SFX_VOICE                     ( 0x21U )
-#define SOUND_CMD_24_UNK                                 ( 0x24U )
-#define SOUND_CMD_30_UNK                                 ( 0x30U )
-#define SOUND_CMD_40_PUSH_MUSIC_STATE                    ( 0x40U )
-#define SOUND_CMD_1A_START_MASTER_AND_MUSIC_VOLUME_FADE  ( 0x1AU )
-#define SOUND_CMD_80_SET_MODE_STEREO                     ( 0x80U )
-#define SOUND_CMD_81_SET_MODE_MONO                       ( 0x81U )
-#define SOUND_CMD_90_SET_MUTED_MUSIC_CHANNELS            ( 0x90U )
-#define SOUND_CMD_92_SET_MUSIC_BRANCH_THRESHHOLD         ( 0x92U )
-#define SOUND_CMD_98_NULL                                ( 0x98U )
-#define SOUND_CMD_99_NULL                                ( 0x99U )
-#define SOUND_CMD_9A_RESTORE_MUSIC                       ( 0x9AU )
-#define SOUND_CMD_9B_SUSPEND_MUSIC                       ( 0x9BU )
-#define SOUND_CMD_9C_RESTORE_SFX                         ( 0x9CU )
-#define SOUND_CMD_9D_SUSPEND_SFX                         ( 0x9DU )
-#define SOUND_CMD_9E_RESTORE_CUTSCENE_AUDIO              ( 0x9EU )
-#define SOUND_CMD_9F_SUSPEND_CUTSCENE_AUDIO              ( 0x9FU )
-#define SOUND_CMD_A0_SET_SFX_VOLUME_MOD                  ( 0xA0U )
-#define SOUND_CMD_A1_FADE_SFX_VOLUME_MOD                 ( 0xA1U )
-#define SOUND_CMD_A2_SET_SFX_PAN_MOD                     ( 0xA2U )
-#define SOUND_CMD_A3_FADE_SFX_PAN_MOD                    ( 0xA3U )
-#define SOUND_CMD_A4_SET_SFX_PITCH_MOD                   ( 0xA4U )
-#define SOUND_CMD_A5_FADE_SFX_PITCH_MOD                  ( 0xA5U )
-#define SOUND_CMD_A8_SET_ALL_SFX_VOLUME_MOD              ( 0xA8U )
-#define SOUND_CMD_A9_FADE_ALL_SFX_VOLUME_MOD             ( 0xA9U )
-#define SOUND_CMD_AA_SET_ALL_SFX_PAN_MOD                 ( 0xAAU )
-#define SOUND_CMD_AB_FADE_ALL_SFX_PAN_MOD                ( 0xABU )
-#define SOUND_CMD_AC_SET_ALL_SFX_PITCH_MOD               ( 0xACU )
-#define SOUND_CMD_AD_FADE_ALL_SFX_PITCH_MOD              ( 0xADU )
-#define SOUND_CMD_C0_SET_MASTER_VOLUME_BY_MUSIC_ID       ( 0xC0U )
-#define SOUND_CMD_C1_FADE_MASTER_VOLUME_BY_MUSIC_ID      ( 0xC1U )
-#define SOUND_CMD_C2_FADE_MASTER_VOLUME_FROM_BY_MUSIC_ID ( 0xC2U )
-#define SOUND_CMD_C4_SET_PAN_BY_MUSIC_ID                 ( 0xC4U )
-#define SOUND_CMD_C5_FADE_PAN_BY_MUSIC_ID                ( 0xC5U )
-#define SOUND_COMMAND_UNK_F0                             ( 0xF0U )
-#define SOUND_COMMAND_UNK_F1                             ( 0xF1U )
+typedef enum ESoundCommand
+{
+    SOUND_CMD_10_START_FIELD_MUSIC                   = 0x10,
+    SOUND_COMMAND_UNK_11                             = 0x11,
+    SOUND_CMD_12_START_FIELD_MUSIC_LOOPED            = 0x12,
+    SOUND_CMD_14_START_BATTLE_MUSIC                  = 0x14,
+    SOUND_CMD_19_SET_MUSIC_LEVEL_IMM                 = 0x19,
+    SOUND_CMD_1A_START_MASTER_AND_MUSIC_VOLUME_FADE  = 0x1A,
+    SOUND_CMD_20_UNK                                 = 0x20,
+    SOUND_CMD_21_EVICT_SFX_VOICE                     = 0x21,
+    SOUND_CMD_24_UNK                                 = 0x24,
+    SOUND_CMD_30_UNK                                 = 0x30,
+    SOUND_CMD_40_PUSH_MUSIC_STATE                    = 0x40,
+    SOUND_CMD_70_SET_CD_VOLUME                       = 0x70,
+    SOUND_CMD_71_FADE_CD_VOLUME                      = 0x71,
+    SOUND_CMD_72_FADE_CD_VOLUME_FROM                 = 0x72,
+    SOUND_CMD_80_SET_MODE_STEREO                     = 0x80,
+    SOUND_CMD_81_SET_MODE_MONO                       = 0x81,
+    SOUND_CMD_90_SET_MUTED_MUSIC_CHANNELS            = 0x90,
+    SOUND_CMD_92_SET_MUSIC_BRANCH_THRESHHOLD         = 0x92,
+    SOUND_CMD_98_NULL                                = 0x98,
+    SOUND_CMD_99_NULL                                = 0x99,
+    SOUND_CMD_9A_RESTORE_MUSIC                       = 0x9A,
+    SOUND_CMD_9B_SUSPEND_MUSIC                       = 0x9B,
+    SOUND_CMD_9C_RESTORE_SFX                         = 0x9C,
+    SOUND_CMD_9D_SUSPEND_SFX                         = 0x9D,
+    SOUND_CMD_9E_RESTORE_CUTSCENE_AUDIO              = 0x9E,
+    SOUND_CMD_9F_SUSPEND_CUTSCENE_AUDIO              = 0x9F,
+    SOUND_CMD_A0_SET_SFX_VOLUME_MOD                  = 0xA0,
+    SOUND_CMD_A1_FADE_SFX_VOLUME_MOD                 = 0xA1,
+    SOUND_CMD_A2_SET_SFX_PAN_MOD                     = 0xA2,
+    SOUND_CMD_A3_FADE_SFX_PAN_MOD                    = 0xA3,
+    SOUND_CMD_A4_SET_SFX_PITCH_MOD                   = 0xA4,
+    SOUND_CMD_A5_FADE_SFX_PITCH_MOD                  = 0xA5,
+    SOUND_CMD_A8_SET_ALL_SFX_VOLUME_MOD              = 0xA8,
+    SOUND_CMD_A9_FADE_ALL_SFX_VOLUME_MOD             = 0xA9,
+    SOUND_CMD_AA_SET_ALL_SFX_PAN_MOD                 = 0xAA,
+    SOUND_CMD_AB_FADE_ALL_SFX_PAN_MOD                = 0xAB,
+    SOUND_CMD_AC_SET_ALL_SFX_PITCH_MOD               = 0xAC,
+    SOUND_CMD_AD_FADE_ALL_SFX_PITCH_MOD              = 0xAD,
+    SOUND_CMD_C0_SET_MASTER_VOLUME_BY_MUSIC_ID       = 0xC0,
+    SOUND_CMD_C1_FADE_MASTER_VOLUME_BY_MUSIC_ID      = 0xC1,
+    SOUND_CMD_C2_FADE_MASTER_VOLUME_FROM_BY_MUSIC_ID = 0xC2,
+    SOUND_CMD_C4_SET_PAN_BY_MUSIC_ID                 = 0xC4,
+    SOUND_CMD_C5_FADE_PAN_BY_MUSIC_ID                = 0xC5,
+    SOUND_COMMAND_UNK_F0                             = 0xF0,
+    SOUND_COMMAND_UNK_F1                             = 0xF1
+
+} ESoundCommand;
 
 // TODO(jperos): What this is
 extern s32 D_80090A00[0x20]; // num == SOUND_CHANNEL_COUNT
